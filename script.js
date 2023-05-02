da35e6959ba813df34bdd16d08af0be327fbdeff
DASARI SAIBABU <107811202+Dasari001@users.noreply.github.com>
2023-05-02 12:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
-var lel = document.getElementBy("level");
-alert(`The level of the element is: ${lel}`);
+var lel = document.getElementById("level");
+alert(`The level of the element is: ${lel}`)
